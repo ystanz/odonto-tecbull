cc46757c89fcc6f4de5dd28cf34aa78affdbdc12
Yuri Stanzani <ystanzani@gmail.com>
2026-07-01 22:20:44 -0300

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
