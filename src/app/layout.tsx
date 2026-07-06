77f7e5938fa5b067a44ef76f9ee4b697a3893e96
Yuri Stanzani <ystanzani@gmail.com>
2026-07-06 09:28:24 -0300

    feat: polimento no UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
       className={`${spaceGrotesk.variable} ${inter.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
       <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
